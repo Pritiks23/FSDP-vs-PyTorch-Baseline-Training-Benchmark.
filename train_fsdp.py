7440c7fea29d2871ab14daa64bd4ef5fbb2a3e34
Pritika Vipin <65793273+Pritiks23@users.noreply.github.com>
2026-06-09 11:18:50 -0700

    Create train_fsdp.py

---
+++ train_fsdp.py
@@ -0,0 +1,37 @@
+import torch
+from torch.utils.data import DataLoader
+from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
+
+from model import get_model
+from dataset import ToyDataset
+from metrics import Timer, gpu_util, mem
+
+def run():
+    model = get_model().cuda()
+    model = FSDP(model)
+
+    opt = torch.optim.AdamW(model.parameters(), lr=1e-4)
+    loader = DataLoader(ToyDataset(), batch_size=2)
+
+    for step, batch in enumerate(loader):
+        batch = {k: v.cuda() for k, v in batch.items()}
+
+        with Timer() as t:
+            loss = model(**batch).loss
+            loss.backward()
+            opt.step()
+            opt.zero_grad()
+
+        print({
+            "step": step,
+            "time_ms": t.dt,
+            "gpu": gpu_util(),
+            "mem": mem(),
+            "loss": loss.item()
+        })
+
+        if step == 50:
+            break
+
+if __name__ == "__main__":
+    run()
